9ab99a612abb02183f1458d905c4c9eab8a39e9b
AndresManrique <af.manrique@uniandes.edu.co>
2020-09-29 21:45:56 -0500

    Sin warnings, error de compilado, sin nav

--- app/src/main/java/com/example/myapplication/ScrollingActivity.java
+++ app/src/main/java/com/example/myapplication/ScrollingActivity.java
@@ -12,20 +12,19 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class ScrollingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vPaciente);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.bienvenida);
+        Toolbar toolbar =  findViewById(R.id.sub_bienvenida_layout);
         setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button_next_1 =  findViewById(R.id.button_next_bienvenida);
+        button_next_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
